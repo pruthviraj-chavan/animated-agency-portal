04b62a27e548624961cecd81331e89e626cdf0bd
Pruthviraj Chavan <91907487+pruthviraj-chavan@users.noreply.github.com>
2025-04-15 23:09:40 +0530

    Update TeamSection.tsx

--- src/components/home/TeamSection.tsx
+++ src/components/home/TeamSection.tsx
@@ -17,7 +17,7 @@ const teamMembers = [
     }
   },
   {
-    name: "Atharva Sankpal",
+    name: "Akash Sakhare",
     role: "Co-founder & COO",
     bio: "Founder - Market-Media Solutions , Expertise In Sales , Buisness Operations",
     image: "/placeholder.svg",
